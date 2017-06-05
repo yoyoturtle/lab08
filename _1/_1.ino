427b9634bfc0cca4115c7656319f7fd3746a301a
yoyoturtle <yoyoturtle@users.noreply.github.com>
2017-06-05 19:36:34 +0800

    Add files via upload

---
+++ _1/_1.ino
@@ -0,0 +1,17 @@
+#include <dht.h>
+#define dht_dpin A0 //定義訊號要從Pin A0 進來
+dht DHT;
+void setup(){
+Serial.begin(9600); delay(300);
+Serial.println("Humidity and temperature\n\n");
+delay(700); }
+
+void loop()
+{
+DHT.read11(dht_dpin); //去library裡面找DHT.read11
+Serial.print("Humidity = "); Serial.print(DHT.humidity);
+Serial.print("% ");
+Serial.print("temperature = ");Serial.print(DHT.temperature);
+Serial.println("C ");
+delay(1000); 
+}
